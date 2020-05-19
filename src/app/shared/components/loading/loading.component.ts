85b63625e553b8644b4ad36711b54a403761a1b5
Xenosgrilda <dualmeidalima@gmail.com>
2020-05-19 18:21:34 -0300

    Loading Visual Update

--- src/app/shared/components/loading/loading.component.ts
+++ src/app/shared/components/loading/loading.component.ts
@@ -3,15 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-loading',
   template: `
-    <div class="animation-container">
-      <div class="loading-bars-container">
-        <div class="loading-bars">
-          <!-- Bars -->
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
+    <div class="background"></div>
+    <div class="loading-bars-container">
+      <div class="loading-bars">
+        <!-- Bars -->
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
     </div>
   `,
